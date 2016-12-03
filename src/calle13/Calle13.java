5b6ce096de8083f269ac4d8261b9291381c013d2
Fabio Fonseca <Fabio Fonseca@10.1.22.81>
2016-12-03 17:28:49 -0500

    Encendido y primer ciclo lavadora

--- src/calle13/Calle13.java
+++ src/calle13/Calle13.java
@@ -18,62 +18,79 @@ public class Calle13 {
      */
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-//        System.out.println("Ingrese alto, ancho, marca y entrada electrica del televisor");
-//        Televisor tv2 = new Televisor(read.nextDouble(),read.nextDouble(),read.next(),read.next());
-        Televisor tv1=new Televisor(35.0, 65.5, "LG","120VAC");//Instancia del televisor
+////        System.out.println("Ingrese alto, ancho, marca y entrada electrica del televisor");
+////        Televisor tv2 = new Televisor(read.nextDouble(),read.nextDouble(),read.next(),read.next());
+//        Televisor tv1=new Televisor(35.0, 65.5, "LG","120VAC");//Instancia del televisor
+////        
+//int opcion;
+//        do {
+//            System.out.println("Ingrese 1 para encender el televisor");
+//            opcion = read.nextInt();
+//            if (opcion==1) {
+//              tv1.enceder();  
+//            }
+//        } while (opcion!=1);
 //        
-int opcion;
+//        
+//        do {
+//            System.out.println("Seleccione una opcion");
+//            tv1.mostrarMenu();
+//            opcion = read.nextInt();
+//            
+//            switch  (opcion){
+//                
+//                case 1:
+//                    tv1.enceder();
+//                    break;
+//                    
+//                case 2:
+//                    tv1.subirVol();
+//                    break;
+//                    
+//                case 3:
+//                    tv1.bajarVolumen();
+//                    break;
+//                    
+//                case 4:
+//                    tv1.subirCanal();
+//                    break;
+//                    
+//                case 5:
+//                    tv1.bajarCanal();
+//                    break;
+//                    
+//                case 6:
+//                    tv1.silenciar();
+//                    break;
+//                    
+//                case 7:
+//                    System.out.println("Ingrese el canal");
+//                    tv1.cambiarCanal(read.nextInt());
+//                    break;              
+//            }
+//            System.out.println(tv1);
+//        } while (opcion!=1);
+//        
+       
+        Lavadora lv1 = new Lavadora("Whirpool", "120VAC");
+        int opcion;
         do {
-            System.out.println("Ingrese 1 para encender el televisor");
+            System.out.println("Ingrese 1 para encender la lavadora");
             opcion = read.nextInt();
             if (opcion==1) {
-              tv1.enceder();  
+              lv1.enceder();  
             }
         } while (opcion!=1);
+        System.out.println(lv1);
         
+        System.out.println("Introduzca la carga a lavar (kg)");
+        int carga = read.nextInt();
+        System.out.println("La carga en su lavadora es de: " + lv1.establecerCarga(carga) + " kg");
+        System.out.println(lv1.establecerCicloLavado(carga));
+        System.out.println("");
         
-        do {
-            System.out.println("Seleccione una opcion");
-            tv1.mostrarMenu();
-            opcion = read.nextInt();
-            
-            switch  (opcion){
-                
-                case 1:
-                    tv1.enceder();
-                    break;
-                    
-                case 2:
-                    tv1.subirVol();
-                    break;
-                    
-                case 3:
-                    tv1.bajarVolumen();
-                    break;
-                    
-                case 4:
-                    tv1.subirCanal();
-                    break;
-                    
-                case 5:
-                    tv1.bajarCanal();
-                    break;
-                    
-                case 6:
-                    tv1.silenciar();
-                    break;
-                    
-                case 7:
-                    System.out.println("Ingrese el canal");
-                    tv1.cambiarCanal(read.nextInt());
-                    break;              
-            }
-            System.out.println(tv1);
-        } while (opcion!=1);
-        
-        
-        
-       
+
+
     }
-    
+
 }
